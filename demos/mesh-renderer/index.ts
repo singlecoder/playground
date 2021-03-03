5b2e16d79bbee911b5ce486c764277c78e67734d
singlecoder <singlecoder2014@gmail.com>
2021-03-03 21:09:40 +0800

    feature: fix uv error

--- demos/mesh-renderer/index.ts
+++ demos/mesh-renderer/index.ts
@@ -1,13 +1,32 @@
 import { OrbitControl } from "@oasis-engine/controls";
 import {
-  BlinnPhongMaterial,
   Camera,
   PrimitiveMesh,
   MeshRenderer,
   SystemInfo,
-  Vector3,
-  WebGLEngine
+  WebGLEngine,
+  Script,
+  Entity,
+  BlinnPhongMaterial,
+  Texture2D,
+  AssetType,
+  DirectLight
 } from "oasis-engine";
+
+class RotateScript extends Script {
+  private _rotateX: number;
+  private _rotateY: number;
+
+  constructor(entity: Entity) {
+    super(entity);
+
+    this._rotateX = 0.5;
+    this._rotateY = 0.6;
+  }
+  onUpdate() {
+    this.entity.transform.rotate(this._rotateX, this._rotateY, 0);
+  }
+}
 
 //-- create engine object
 const engine = new WebGLEngine("o3-demo");
@@ -19,45 +38,81 @@ const rootEntity = scene.createRootEntity();
 
 //-- create camera
 const cameraEntity = rootEntity.createChild("camera_entity");
-cameraEntity.transform.position = new Vector3(0, 0, 50);
+cameraEntity.transform.setPosition(0, 0, 50);
 cameraEntity.addComponent(Camera);
 cameraEntity.addComponent(OrbitControl);
 
-//-- Create cuboid
-const cuboidEntity = rootEntity.createChild("cuboid");
-cuboidEntity.transform.setPosition(-5, 5, 0);
-cuboidEntity.transform.rotate(30, 10, 80);
-const cuboidRenderer = cuboidEntity.addComponent(MeshRenderer);
-const cuboidMaterial = new BlinnPhongMaterial(engine);
-cuboidMaterial.emissiveColor.setValue(1, 0, 0, 1);
-cuboidRenderer.mesh = PrimitiveMesh.createCuboid(engine, 3, 3, 3);
-cuboidRenderer.setMaterial(cuboidMaterial);
+//-- create direct light
+const lightEntity = rootEntity.createChild("DirectLight");
+lightEntity.transform.setPosition(-30, 20, 10);
+const light = lightEntity.addComponent(DirectLight);
+light.color.setValue(1, 1, 1, 1);
+light.intensity = 0.3;
 
-//-- Create sphere
-const sphereEntity = rootEntity.createChild("sphere");
-sphereEntity.transform.setPosition(5, 5, 0);
-const sphereRenderer = sphereEntity.addComponent(MeshRenderer);
-const sphereMaterial = new BlinnPhongMaterial(engine);
-sphereMaterial.emissiveColor.setValue(0, 1, 0, 1);
-sphereRenderer.mesh = PrimitiveMesh.createSphere(engine, 3);
-sphereRenderer.setMaterial(sphereMaterial);
+const distanceX = 8;
+const distanceY = 8;
 
-//-- Create plane
-const planeEntity = rootEntity.createChild("plane");
-planeEntity.transform.setPosition(-5, -5, 0);
-const planeRenderer = planeEntity.addComponent(MeshRenderer);
-const planeMaterial = new BlinnPhongMaterial(engine);
-planeMaterial.emissiveColor.setValue(0, 0, 1, 1);
-planeRenderer.mesh = PrimitiveMesh.createPlane(engine, 3, 3);
-planeRenderer.setMaterial(planeMaterial);
+engine.resourceManager
+  .load<Texture2D>({
+    url: "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*ArCHTbfVPXUAAAAAAAAAAAAAARQnAQ",
+    type: AssetType.Texture2D
+  })
+  .then((resource) => {
+    for (let i = 0; i < 3; ++i) {
+      const posX = (i - 1) * distanceX;
 
-//-- Create cylinder
-const cylinderEntity = rootEntity.createChild("cylinder");
-cylinderEntity.transform.setPosition(5, -5, 0);
-const cylinderRenderer = cylinderEntity.addComponent(MeshRenderer);
-const cylinderMaterial = new BlinnPhongMaterial(engine);
-cylinderMaterial.emissiveColor.setValue(1, 1, 1, 1);
-cylinderRenderer.mesh = PrimitiveMesh.createCylinder(engine, 1, 5);
-cylinderRenderer.setMaterial(cylinderMaterial);
+      //-- Create cuboid
+      const cuboidEntity = rootEntity.createChild("cuboid");
+      const cuboidTransform = cuboidEntity.transform;
+      cuboidTransform.setPosition(posX, distanceY * 1.5, 0);
+      cuboidTransform.setScale(3, 3, 3);
+      cuboidEntity.addComponent(RotateScript);
+      const cuboidRenderer = cuboidEntity.addComponent(MeshRenderer);
+      const cuboidMaterial = new BlinnPhongMaterial(engine);
+      cuboidMaterial.emissiveTexture = resource;
+      cuboidMaterial.emissiveColor.setValue(1, 1, 1, 1);
+      cuboidRenderer.mesh = PrimitiveMesh.createCuboid(engine);
+      cuboidRenderer.setMaterial(cuboidMaterial);
+
+      //-- Create sphere
+      const sphereEntity = rootEntity.createChild("sphere");
+      const sphereTransform = sphereEntity.transform;
+      sphereTransform.setPosition(posX, distanceY * 0.5, 0);
+      sphereTransform.setScale(3, 3, 3);
+      sphereEntity.addComponent(RotateScript);
+      const sphereRenderer = sphereEntity.addComponent(MeshRenderer);
+      const sphereMaterial = new BlinnPhongMaterial(engine);
+      sphereMaterial.emissiveTexture = resource;
+      sphereMaterial.emissiveColor.setValue(1, 1, 1, 1);
+      sphereRenderer.mesh = PrimitiveMesh.createSphere(engine, 0.5, (i + 1) * 6);
+      sphereRenderer.setMaterial(sphereMaterial);
+
+      //-- Create plane
+      const planeEntity = rootEntity.createChild("plane");
+      const planeTransform = planeEntity.transform;
+      planeTransform.setPosition(posX, -distanceY * 0.5, 0);
+      planeTransform.setScale(3, 3, 3);
+      planeEntity.addComponent(RotateScript);
+      const planeRenderer = planeEntity.addComponent(MeshRenderer);
+      const planeMaterial = new BlinnPhongMaterial(engine);
+      planeMaterial.emissiveTexture = resource;
+      planeMaterial.emissiveColor.setValue(1, 1, 1, 1);
+      planeRenderer.mesh = PrimitiveMesh.createPlane(engine);
+      planeRenderer.setMaterial(planeMaterial);
+
+      //-- Create cylinder
+      const cylinderEntity = rootEntity.createChild("cylinder");
+      const cylinderTransform = cylinderEntity.transform;
+      cylinderTransform.setPosition(posX, -distanceY * 1.5, 0);
+      cylinderTransform.setScale(3, 3, 3);
+      cylinderEntity.addComponent(RotateScript);
+      const cylinderRenderer = cylinderEntity.addComponent(MeshRenderer);
+      const cylinderMaterial = new BlinnPhongMaterial(engine);
+      cylinderMaterial.emissiveTexture = resource;
+      cylinderMaterial.emissiveColor.setValue(1, 1, 1, 1);
+      cylinderRenderer.mesh = PrimitiveMesh.createCylinder(engine, 0.5, 2, (i + 1) * 6, 1);
+      cylinderRenderer.setMaterial(cylinderMaterial);
+    }
+  });
 
 engine.run();
